9baab1afe844d748acbe5e5445f9ab79a04c399b
Peng <pli9@bwh.harvard.edu>
2020-06-02 06:59:17 -0400

    publish detGap

---
+++ detGap.m
@@ -0,0 +1,24 @@
+function [gap, res, fmt] = detGap(actigraphy, epoch)
+%DETGAP detection gap
+%   
+%   GAP = DETGAP(ACTIGRAPHY, EPOCH) detects gaps in ACTIGRAPHY with epoch
+%       length EPOCH second
+%
+%   $Author:    Peng Li
+% 
+
+minDuration = 60 / epoch * 60 * 2;
+gapseries   = actigraphy <= 0;
+
+if any(gapseries)
+    gap = DetConstantOne(gapseries);
+    gap(gap(:, 2) - gap(:, 1) < minDuration) = [];
+else
+    gap = [];
+end
+
+% request results
+aLenInDay = length(actigraphy) * epoch / 60 / 60 / 24;
+gapPerc   = sum(gap(:, 2) - gap(:, 1) + 1) / length(actigraphy);
+res = [aLenInDay, gapPerc];
+fmt = '%d\t%f\r';
