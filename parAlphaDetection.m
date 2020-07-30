7f3f517f5092c2997779cf64a7ade676f6951d8d
Peng <pli9@bwh.harvard.edu>
2020-07-30 12:48:50 -0400

    publish parAlphaDetection.m

---
+++ parAlphaDetection.m
@@ -0,0 +1,64 @@
+function parAlphaDetection(source, destination, epoch, option)
+%PARALPHADETECTION do dfa in activity files using parallel process
+%
+%   PARALPHADETECTION(SOURCE, DESTINATION, EPOCH, OPTION) run dfa on all
+%   files stored in SOURCE and save results in seperate files in
+%   DESTINATION. EPOCH is a scalar that specifies the epoch of all files. 
+%   OPTION is a struct specifying region and starttime.
+%
+%   $Author:    Peng Li
+%
+
+allFiles = dir(fullfile(source, '*.txt'));
+
+fprintf('==\tDFA PROC\r');
+fprintf('==\t%d files to process\r', numel(allFiles));
+
+p = gcp('nocreate');
+if isempty(p)
+    p = parpool;
+    poolsize = p.NumWorkers;
+else
+    poolsize = p.NumWorkers;
+end
+fprintf('==\tdistributed into %d workers\r', poolsize);
+
+% q the status within parfor
+q = parallel.pool.DataQueue;
+q.afterEach(@(x) prog(x));
+
+% create unique file id within each worker
+c = parallel.pool.Constant(@() fopen(tempname(destination), 'wt'), @fclose);
+spmd
+    A = fopen(c.Value);
+end
+
+parfor idx = 1:numel(allFiles)
+    curFile = fullfile(source, allFiles(idx).name);
+    runFile('detAlpha', curFile, destination, epoch, c.Value, option);
+    
+    send(q, idx);
+end
+
+% throw c to run fclose
+clear c;
+
+% merge
+fprintf('==\tstarting consolidation\r');
+spmd
+    tblLab = readtable(A, 'ReadVariableNames', 0, ...
+        'Delimiter', '\t');
+end
+tbl = vertcat(tblLab{:});
+tbl.Properties.VariableNames = {'filename', 'minscale', 'maxscale', 'alpha', 'goodness', 'intercept'};
+writetable(tbl, fullfile(destination, 'alpha_stat.sum'), 'FileType', 'text');
+fprintf('==\tresults consolidated\r');
+
+fprintf('==\tFINISHED\r');
+end
+
+function prog(x)
+if mod(x, 500) == 0
+    fprintf('==\t%d done\r', x)
+end
+end
