4cca69a28d0dfe4d968cd6ab3f51b9fb4d6e5477
Peng <pli9@bwh.harvard.edu>
2020-12-03 12:26:38 -0500

    update calculations for numbers of awakenings to take 0 sleep into consideration

--- detSleep.m
+++ detSleep.m
@@ -1,9 +1,9 @@
 function [sleep, res, fmt] = detSleep(actigraphy, epoch, varargin)
 %DETSLEEP do sleep detection
 %   
-%   [SLEEP, RES, FMT] = DETSLEEP(ACTIGRAPHY, EPOCH, STARTTIME, QUALITY)
+%   [SLEEP, RES, FMT] = DETSLEEP(ACTIGRAPHY, EPOCH, STARTTIME, QUALITY, WINDOW, PARAMETER)
 %       performs sleep detection on ACTIGRAPHY of epoch length EPOCH and 
-%       return results to the caller
+%       return results to the caller.
 %       FMT request the format to write RES to file
 %       SLEEP is the actual sleep episodes to be saved
 %
@@ -12,14 +12,18 @@ function [sleep, res, fmt] = detSleep(actigraphy, epoch, varargin)
 % 
 
 % to be modified
-narginchk(4, 4);
+narginchk(6, 6);
 
 startTime  = varargin{1};
 a = acti(actigraphy, 'Epoch', epoch, 'StartTime', startTime);
 a.GapSeries = ~varargin{2};
 
-a.SleepInfo = struct('StartTime', '21:00', 'EndTime', '7:00', ...
-    'ModeParameter', struct('P', 0.001, 'V', [106 54 58 76 230 74 67], 'C', 0));
+window = varargin{3};
+para   = varargin{4};
+
+a.SleepInfo = struct('StartTime', window.StartTime, ...
+    'EndTime', window.EndTime, ...
+    'ModeParameter', para);
 
 a = a.sleepDet;
 
