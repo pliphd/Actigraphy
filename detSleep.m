5238dc8060c48bcc10935fd4f52c64b732f5d82e
Peng <pli9@bwh.harvard.edu>
2021-04-22 11:57:51 -0400

    modify to perform analysis with at least two days of data

--- detSleep.m
+++ detSleep.m
@@ -9,27 +9,34 @@ function [sleep, res, fmt] = detSleep(actigraphy, epoch, varargin)
 %
 %   $Author:    Peng Li
 %   $Date:      Dec 3, 2020
+%   $Modif.:    Apr 22, 2021
+%                   restricted to at least 2 days of data
 % 
 
 % to be modified
 narginchk(6, 6);
 
-startTime  = varargin{1};
-a = acti(actigraphy, 'Epoch', epoch, 'StartTime', startTime);
-a.GapSeries = ~varargin{2};
-
-window = varargin{3};
-para   = varargin{4};
-
-a.SleepInfo = struct('StartTime', window.StartTime, ...
-    'EndTime', window.EndTime, ...
-    'ModeParameter', para);
-
-a = a.sleepDet;
-
-% request results
-sleep = a.Sleep;
-
-% request summary results here
-res = a.SleepSummary.Report{:, :};
+if length(actigraphy)*epoch / 3600 / 24 >= 2
+    startTime  = varargin{1};
+    a = acti(actigraphy, 'Epoch', epoch, 'StartTime', startTime);
+    a.GapSeries = ~varargin{2};
+    
+    window = varargin{3};
+    para   = varargin{4};
+    
+    a.SleepInfo = struct('StartTime', window.StartTime, ...
+        'EndTime', window.EndTime, ...
+        'ModeParameter', para);
+    
+    a = a.sleepDet;
+    
+    % request results
+    sleep = a.Sleep;
+    
+    % request summary results here
+    res = a.SleepSummary.Report{:, :};
+else
+    sleep = [nan nan];
+    res   = [nan nan];
+end
 fmt = '%.2f\t%.2f\r';
