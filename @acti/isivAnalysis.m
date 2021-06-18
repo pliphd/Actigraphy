852eccd87c5c5edba81834a19ae5a639ef6fad98
Peng <pli9@bwh.harvard.edu>
2021-06-18 12:50:07 -0400

    publish isiv function

---
+++ @acti/isivAnalysis.m
@@ -0,0 +1,27 @@
+function this = isivAnalysis(this)
+%ISIVANALYSIS Perform nonparametric ISIV analysis on an ACTI object
+% 
+% $Author:  Peng Li
+% $Date:    Jun 15, 2020
+% 
+
+period = this.ISIVInfo.PeriodInHour;
+epoch  = this.Epoch / 60; % in min
+cycle  = this.ISIVInfo.FixedCycles;
+x      = this.Data;
+x(this.GapSeries) = nan;
+
+nonparaTbl = cell(numel(this.ISIVInfo.TimeScaleInMin), 1);
+for iT = 1:numel(this.ISIVInfo.TimeScaleInMin)
+    scale  = this.ISIVInfo.TimeScaleInMin(iT);
+    
+    [is, iv, current_period, perc_missing_data, perc_missing_bin] = ...
+        isiv(x, epoch, scale, period, cycle);
+    current_period = current_period(:);
+    
+    tbl = table(is, iv, current_period, perc_missing_data, perc_missing_bin);
+    tbl.timescale = scale .* ones(numel(is), 1);
+    
+    nonparaTbl{iT} = tbl;
+end
+this.ISIVSummary = vertcat(nonparaTbl{:});
